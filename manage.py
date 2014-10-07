972426115d59ef1bb510a033e1c2a6c0bc48f0be
Samael500 <samael500@gmail.com>
2014-10-07 20:31:50 +0400

    refactor: change middaygun tweet time

--- manage.py
+++ manage.py
@@ -11,20 +11,18 @@ scheduler = BlockingScheduler()
 twitter = sevboats.Twitter()
 middaygun = sevboats.MiddayGun()
 
-# fed functions
-
-@scheduler.scheduled_job('cron', hour='12', id='middaygun')
+# def functions
+@scheduler.scheduled_job('cron', hour='12', second='5', id='middaygun')
 def middaygun_msg():
     """ Post middaygun messages """
     return twitter.post_tweet(middaygun.get_message())
 
 
-
 if __name__ == '__main__':
     # print (sys.argv)
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    print 'Press Ctrl+C to exit'
 
     try:
         scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
+    except KeyboardInterrupt, SystemExit:
         pass
