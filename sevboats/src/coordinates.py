07eb6b8569b7fa21ec5c9249cfaa12affd7e3ded
Samael500 <samael500@gmail.com>
2014-10-12 13:16:10 +0400

    test: coordinat test

--- sevboats/src/coordinates.py
+++ sevboats/src/coordinates.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from math import pi, sin, cos
+from math import pi, sin, cos, acos, sqrt
 
 
 class Coordinates:
@@ -16,10 +16,14 @@ class Coordinates:
         self.latitude = latitude
         self.longitude = longitude
 
-    def square(a, b, c):
-        """ Calculate the 2area of ​​a triangle for given points - matrix solution calc det with out div 2 """
+    def det(a, b, c):
+        """ Calculate determinant for given points """
         return a.latitude * (b.longitude - c.longitude) + b.latitude * (c.longitude - a.longitude) + \
             c.latitude * (a.longitude - b.longitude)
+
+    def square(a, b, c):
+        """ Calculate the area of a triangle for given points - matrix solution calc det with div 2 """
+        return abs(Coordinates.det(a, b, c)) / 2.
 
     def __sub__(self, point):
         """ Return self A - coord B """
@@ -46,6 +50,11 @@ class Coordinates:
         """ Return self A != coord B """
         return not (self == point)
 
+    def __neg__(self):
+        """ Return -self coord """
+        return Coordinates(- self.latitude, - self.longitude)
+
+    @property
     def length(self):
         """ Get vector length - by self coordinates """
         return sqrt(self.latitude * self.latitude + self.longitude * self.longitude)
@@ -53,33 +62,34 @@ class Coordinates:
     def __str__(self):
         return "latitude: {0}, longitude: {1}".format(self.latitude, self.longitude)
 
+    def scalar(start, dest, course):
+        # phi = cours to radian Decart angle
+        phi = (450 - course) % 360  # grad
+        phi = pi * phi / 180        # rad
+        # create vectors
+        A = Coordinates(sin(phi), cos(phi))
+        B = dest - start
+        # calculate scalar
+        return A * B / (A.length * B.length)
+
     def angle(self, course, dest):
         """
         Calculates the angle between the vectors
         from the point "self" to the point "dest"
         and from the point of "self" with angle "course"
         """
-        # phi = cours to radian Decart angle
-        phi = (450 - course) % 360  # grad
-        phi = pi * phi / 180        # rad
-
-        A = Coordinates(sin(phi), cos(phi))
-        B = dest - self
-
-        scalar = A * B / (A.length() * B.length())
-
-        co = acos(scalar) / pi * 180
-
-        return co
+        scalar = Coordinates.scalar(self, dest, course)
+        return acos(scalar) / pi * 180
 
     def collinear(self, course, start):
         """ Return bool is a vectors are collinear """
-        # phi = cours to radian Decart angle
-        phi = (450 - course) % 360  # grad
-        phi = pi * phi / 180        # rad
-
-        A = Coordinates(sin(phi), cos(phi))
-        B = self - start
-        scalar = A * B / (A.length() * B.length())
-
+        scalar = Coordinates.scalar(start, self, course)
         return scalar > 0
+
+    @property
+    def lat(self):
+        return self.latitude
+
+    @property
+    def long(self):
+        return self.longitude
