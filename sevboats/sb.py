1268079b34370c5a840b0ee61ccc74d98535df34
Samael500 <samael500@gmail.com>
2014-10-18 20:34:56 +0400

    feature: add check routes and update lastpos

--- sevboats/sb.py
+++ sevboats/sb.py
@@ -1,13 +1,23 @@
-from src.engine import SL
+from src.engine import *
+from src.coordinates import Coordinates
+
+loadship()
+update_ais_data()
+update_ais_lastpos()
+
+SlL.fleet().print_ships(True)
+print 'on routes: ', check_ships_on_routes()
+
+# for mmsi, ship in SlL.fleet().container.iteritems():
+#     print 
 
 
-# SL.ais_scrapper()
-
-# SL.fleet().get_ships()
-# SL.fleet().get_ships()
-# SL.fleet().get_ships()
-
-# SL.fleet().print_ships()
+    # print len(ship.lastpos)
 
 
-print unicode(SL.routes().deadend)
+# from src.scrapper import Scrapper
+
+# sr = Scrapper()
+
+# lastpos = sr.scrape_lastpos('272083800')
+# print lastpos
--- sevboats/src/engine.py
+++ sevboats/src/engine.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .ships import ShipsContainer, Ship
-from .shiptracks import RoutesContainer
-from .scrapper import Scrapper
-
-
 class SingletonLazy(object):
 
     @staticmethod
@@ -25,16 +20,50 @@ class SingletonLazy(object):
     def routes():
         return SingletonLazy.get(RoutesContainer)
 
+    @staticmethod
+    def deadend():
+        return SingletonLazy.get(RoutesContainer).deadend
 
-SL = SingletonLazy
+
+SlL = SingletonLazy
+
+
+from .ships import ShipsContainer, Ship
+from .shiptracks import RoutesContainer
+from .scrapper import Scrapper
 
 
 def loadship():
     """ Load ship info from data/ships.yaml """
-    SlL.fleet.get_ships()
+    SlL.fleet().get_ships()
 
 def update_ais_data():
     """ Request ais to get ship data and update it """
-    mmsis = SlL.fleet.mmsi_list
-    data = SlL.ais_scrapper.scrape_ships_list(mmsis)
-    SlL.fleet.update_ships(data)
+    mmsis = SlL.fleet().mmsi_list
+    data = SlL.ais_scrapper().scrape_ships_list(mmsis)
+    SlL.fleet().update_ships(data)
+
+def update_ais_lastpos():
+    """ Request ais to get ship lastpos """
+    mmsis = SlL.fleet().get_online_mmsis()
+    data = SlL.ais_scrapper().scrape_ships_list_lastpos(mmsis)
+    SlL.fleet().update_ships_lastpos(data)
+
+def check_ships_on_routes():
+    """ Check how many ship on routes are moved in last hours """
+    moved_on_routes = 0
+    for mmsi, ship in SlL.fleet().container.iteritems():
+        # run odometr and get distance
+        distance = ship.odometer()
+        # creacte checkers
+        verification_max = -1
+        route_index = None
+        for index, route in enumerate(SlL.routes().container):
+            verification = route.verification(ship)
+            if verification > verification_max:
+                verification_max = verification
+                route_index = index
+        # if ship moving and it on same route
+        if (route_index is not None) and (distance > Ship.MOVING_LENGTH):
+            moved_on_routes += 1
+    return moved_on_routes
